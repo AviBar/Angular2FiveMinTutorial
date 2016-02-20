c457f3a50e5ca75381270ee38d3b2ce8f5bac769
AviBar <barnesavitan@gmail.com>
2016-02-20 21:15:22 +0200

    Added files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1 style="font-size: 110px;color:red;">Get this gulp ya!!!!</h1>'
+})
+export class AppComponent { }
